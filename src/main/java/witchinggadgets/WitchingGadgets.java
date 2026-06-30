b51deb33a215c8991f3cbed6efa9436027948b48
Thenemex <tnmx777@gmail.com>
2026-06-30 06:58:14 +0200

    Changed version

--- src/main/java/witchinggadgets/WitchingGadgets.java
+++ src/main/java/witchinggadgets/WitchingGadgets.java
@@ -44,7 +44,7 @@ public class WitchingGadgets
 {
 	public static final String MODID = "WitchingGadgets";
 	public static final String MODNAME = "Witching Gadgets : Patched";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.2.0.1";
 
 	public PlayerTickHandler playerTickHandler;
 
