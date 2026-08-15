885bb6604028593e612f61ec1aaec385b6b4c598
Thenemex <tnmx777@gmail.com>
2026-08-15 04:26:06 +0200

    Set version to 1.2.1

--- src/main/java/witchinggadgets/WitchingGadgets.java
+++ src/main/java/witchinggadgets/WitchingGadgets.java
@@ -45,7 +45,7 @@ public class WitchingGadgets
 {
 	public static final String MODID = "WitchingGadgets";
 	public static final String MODNAME = "Witching Gadgets : Patched";
-	public static final String VERSION = "1.2.0.1";
+	public static final String VERSION = "1.2.1";
 
 	public PlayerTickHandler playerTickHandler;
 
