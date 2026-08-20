4e747a0e8eefa5654304b496c80559882396e715
Thenemex <tnmx777@gmail.com>
2026-08-20 06:18:22 +0200

    Set version to 1.2.1.1

--- src/main/java/witchinggadgets/WitchingGadgets.java
+++ src/main/java/witchinggadgets/WitchingGadgets.java
@@ -45,7 +45,7 @@ public class WitchingGadgets
 {
 	public static final String MODID = "WitchingGadgets";
 	public static final String MODNAME = "Witching Gadgets : Patched";
-	public static final String VERSION = "1.2.1";
+	public static final String VERSION = "1.2.1.1";
 
 	public PlayerTickHandler playerTickHandler;
 
@@ -154,7 +154,7 @@ public class WitchingGadgets
 	public static final String dependencies =
 			"required-after:Thaumcraft;" +
 			"required-after:TravellersGear@[1.16.4,);" +
-			"required-after:NemexLib@[1.11.4,);" +
+			"required-after:NemexLib@[1.11.5,);" +
 			"after:TwilightForest;" +
 			"after:Mystcraft;" +
 			"after:TConstruct;" +
